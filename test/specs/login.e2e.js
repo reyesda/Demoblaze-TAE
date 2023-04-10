ac1f85c2b5ce268876828feb1182de1bfb4432e9
reyesda <54370848+reyesda@users.noreply.github.com>
2023-04-10 09:42:14 -0500

    Added test cases for R2

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,19 +1,95 @@
 const LoginPage = require('../pageobjects/login.page')
 const Alert = require('../pageobjects/alert.page')
 
-describe('R1 User sign up', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
-        await LoginPage.openLoginWindow()
-        await expect(LoginPage.LoginTitle).toBeDisplayed()
+describe('R2 User Login ', () => {
+    describe('Susccesful login', async () => {
+        it('R2TC1 Pop up login window', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+            await LoginPage.buttonCloseWindow.click()
+        })
+        
+        it('R2TC2 Successful login  ', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
 
-        await LoginPage.login(' ','k')
-        
-        const alertText = await Alert.getAlertMesage(); //get the alert text
-        
-        console.log('finished alert',alertText)
-        
+            let username = '00000test5'
+
+            await LoginPage.login(username,username)
+            
+            await expect(LoginPage.usernamelogedtitle).toHaveTextContaining(username)
+        })
+    })
+
+    describe('Unsuccessful login', async () => {
+        it('R2TC3 Unsuccessful login by empty username and password', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+    
+            await LoginPage.login('','')
+            
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            expect(alertText).toEqual('Please fill out Username and Password.')
+        })
+
+        it('R2TC4 Unsuccessful login by empty password', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+    
+            let username = '00000test5'
+
+            await LoginPage.login(username,'')
+            
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            expect(alertText).toEqual('Please fill out Username and Password.')
+        })
+
+        it('R2TC5 Unsuccessful login by empty username', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+    
+            let username = '00000test5'
+
+            await LoginPage.login('',username)
+            
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            expect(alertText).toEqual('Please fill out Username and Password.')
+        })
+
+        it('R2TC6 Unsuccessful login by wrong password and correct username', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+    
+            let username = '00000test5'
+
+            await LoginPage.login(username,'1')
+            
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            expect(alertText).toEqual('Wrong password.')
+        })
+
+        it('R2TC7 Unsuccessful login by unexist username', async () => {
+            await LoginPage.open()
+            await LoginPage.openLoginWindow()
+            await expect(LoginPage.LoginTitle).toBeDisplayed()
+    
+            let username = '00000test5'
+
+            await LoginPage.login(username+username,username)
+            
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            expect(alertText).toEqual('User does not exist.')
+        })
     })
 })
-
-
--- test/specs/signup.e2e.js
+++ test/specs/signup.e2e.js
@@ -3,10 +3,6 @@ const Alert = require('../pageobjects/alert.page')
 
 describe('R1 User sign up', async () => {
     describe('Successful sing up', async () => {
-        
-        afterEach('close signup pop up window', async () => {
-            await Signup.buttonCloseWindow.click()
-        })
 
         it('R1TC1 Pop up sign upwindow', async () => {
             await Signup.open()
@@ -15,31 +11,45 @@ describe('R1 User sign up', async () => {
             await Signup.buttonCloseWindow.click()
         })
 
-        it('R1TC2 Successful sing up', async () => {
+        it.skip('R1TC2 Successful sing up', async () => {
             await Signup.open()
             await Signup.openSignupWindow()
+            await expect(Signup.SignupTitle).toBeDisplayed()
+    
+            let usernameTest = '00000test' + 8
 
-            await Signup.signup('00000test0','00000test0')
+            await Signup.signup(usernameTest, usernameTest)
             
-            const alertText = await Alert.getAlertMesage();
-
-            expect(alertText).toEqual('Sign up successful.')
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            await expect(alertText).toEqual('Sign up successful.')
             
         })
     })
 
     describe('Unsuccessful sing up', async () => {
-        it('Successful sing up', async () => {
+        it.skip('R1TC3 Sign up validation password', async () => {
             await Signup.open()
             await Signup.openSignupWindow()
             await expect(Signup.SignupTitle).toBeDisplayed()
     
-            await Signup.signup('k','k')
+            await Signup.signup('00000test6','1')
             
             const alertText = await Alert.getAlertMesage(); //get the alert text
             
-            console.log('finished alert',alertText)
+            await expect(alertText).toEqual('Invalid password.')
+        })
+        
+        it('R1TC4 Unsuccessful sing up', async () => {
+            await Signup.open()
+            await Signup.openSignupWindow()
+            await expect(Signup.SignupTitle).toBeDisplayed()
+    
+            await Signup.signup('00000test0','00000test0')
             
+            const alertText = await Alert.getAlertMesage(); //get the alert text
+            
+            await expect(alertText).toEqual('This user already exist.')
         })
     })
 })
